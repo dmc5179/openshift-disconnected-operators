ba8e08da1d7c6a52e2dfe0f77fcb8d9ef87a23b2
Dan Clark <danclark@redhat.com>
2021-11-10 21:53:25 -0500

    Helper script for mirroring a single operator

--- single_operator.sh
+++ single_operator.sh
@@ -1,41 +1,50 @@
-#!/bin/bash
+#!/bin/bash -e
 
-OCP_VERSION="4.8"
+OCP_VERSION="4.9"
 # redhat, certified, community
 CATALOG="redhat"
-OPERATOR="compliance-operator"
+#CATALOG="certified"
+#CATALOG="community"
+OPERATORS="advanced-cluster-management aws-efs-csi-driver-operator cincinnati-operator cluster-logging compliance-operator container-security-operator file-integrity-operator elasticsearch-operator local-storage-operator nfd ocs-operator openshift-gitops-operator openshift-special-resource-operator openshift-pipelines-operator-rh openshift-jenkins-operator quay-bridge-operator quay-operator rhacs-operator rhsso-operator"
 REGISTRY="bastion.danclark.io:5000"
 DIR="/opt/openshift/mirror"
 
-./mirror-operator-catalogue.py \
-  --authfile /opt/openshift/pull-secret.txt \
-  --ocp-version ${OCP_VERSION} \
-  --operator-channel ${OCP_VERSION} \
-  --operator-catalog-image-url registry.redhat.io/redhat/${CATALOG}-operator-index \
-  --operator-list ${OPERATOR} \
-  --custom-operator-catalog-image-and-tag redhat/${OPERATOR}-catalog:v${OCP_VERSION} \
-  --registry-olm ${REGISTRY} \
-  --registry-catalog ${REGISTRY} \
-  --to-dir=${DIR} \
-  --mirror-images false
+for operator in ${OPERATORS}
+do
 
-OPERATOR_VERSION=$(grep Version ./publish/mirror_log.txt | awk '{print $NF}' | tr -d ']')
-rm -rf ./publish
-rm -rf ${DIR}
-mkdir ${DIR}
+  ./mirror-operator-catalogue.py \
+    --authfile /opt/openshift/pull-secret.txt \
+    --ocp-version ${OCP_VERSION} \
+    --operator-channel ${OCP_VERSION} \
+    --operator-catalog-image-url registry.redhat.io/redhat/${CATALOG}-operator-index \
+    --operator-list ${operator} \
+    --custom-operator-catalog-image-and-tag redhat/${operator}-catalog:v${OCP_VERSION} \
+    --registry-olm ${REGISTRY} \
+    --registry-catalog ${REGISTRY} \
+    --to-dir=${DIR} \
+    --mirror-images false
 
-./mirror-operator-catalogue.py \
-  --authfile /opt/openshift/pull-secret.txt \
-  --ocp-version ${OCP_VERSION} \
-  --operator-channel ${OCP_VERSION} \
-  --operator-catalog-image-url registry.redhat.io/redhat/${CATALOG}-operator-index \
-  --operator-list ${OPERATOR} \
-  --custom-operator-catalog-image-and-tag redhat/${OPERATOR}-catalog:v${OPERATOR_VERSION} \
-  --registry-olm ${REGISTRY} \
-  --registry-catalog ${REGISTRY} \
-  --to-dir=${DIR} \
+  OPERATOR_VERSION=$(grep Version ./publish/mirror_log.txt | awk '{print $NF}' | tr -d ']')
+  rm -rf ./publish
+  rm -rf ${DIR}
+  mkdir ${DIR}
 
-tar -czf "${OPERATOR}-${OPERATOR_VERSION}-mapping.tgz" ./publish
-rm -rf publish
-tar -cf "${OPERATOR}-${OPERATOR_VERSION}-images.tar" "${DIR}"
-rm -rf "${DIR}"
+  ./mirror-operator-catalogue.py \
+    --authfile /opt/openshift/pull-secret.txt \
+    --ocp-version ${OCP_VERSION} \
+    --operator-channel ${OCP_VERSION} \
+    --operator-catalog-image-url registry.redhat.io/redhat/${CATALOG}-operator-index \
+    --operator-list ${operator} \
+    --custom-operator-catalog-image-and-tag redhat/${operator}-catalog:v${OPERATOR_VERSION} \
+    --registry-olm ${REGISTRY} \
+    --registry-catalog ${REGISTRY} \
+    --to-dir=${DIR} \
+
+  tar -czf "${operator}-${OPERATOR_VERSION}-mapping.tgz" ./publish
+  rm -rf publish
+  tar -cf "${operator}-${OPERATOR_VERSION}-images.tar" "${DIR}"
+  rm -rf "${DIR}"
+
+done
+
+exit 0
