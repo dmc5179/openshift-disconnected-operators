48697964aef2ed503652785620fa45ed4b26b18b
Dan Clark <danclark@redhat.com>
2022-01-12 03:15:37 +0000

    Simplify helper script

--- single_operator.sh
+++ single_operator.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-OCP_VERSION="4.9"
+OCP_VERSION="4.7"
 # redhat, certified, community
 CATALOG="redhat"
 #CATALOG="certified"
 #CATALOG="community"
-OPERATORS="advanced-cluster-management aws-efs-csi-driver-operator cincinnati-operator cluster-logging compliance-operator container-security-operator file-integrity-operator elasticsearch-operator local-storage-operator nfd ocs-operator openshift-gitops-operator openshift-special-resource-operator openshift-pipelines-operator-rh openshift-jenkins-operator quay-bridge-operator quay-operator rhacs-operator rhsso-operator"
+OPERATORS="elasticsearch-operator"
 REGISTRY="bastion.danclark.io:5000"
 DIR="/opt/openshift/mirror"
 PULL_SECRET="/home/ec2-user/pull-secret.txt"
@@ -13,19 +13,6 @@ PULL_SECRET="/home/ec2-user/pull-secret.txt"
 for operator in ${OPERATORS}
 do
 
-  ./mirror-operator-catalogue.py \
-    --authfile ${PULL_SECRET} \
-    --ocp-version ${OCP_VERSION} \
-    --operator-channel ${OCP_VERSION} \
-    --operator-catalog-image-url registry.redhat.io/redhat/${CATALOG}-operator-index \
-    --operator-list ${operator} \
-    --custom-operator-catalog-image-and-tag redhat/${operator}-catalog:v${OCP_VERSION} \
-    --registry-olm ${REGISTRY} \
-    --registry-catalog ${REGISTRY} \
-    --to-dir=${DIR} \
-    --mirror-images false
-
-  OPERATOR_VERSION=$(grep Version ./publish/mirror_log.txt | awk '{print $NF}' | tr -d ']')
   rm -rf ./publish
   rm -rf ${DIR}
   mkdir ${DIR}
@@ -40,6 +27,8 @@ do
     --registry-olm ${REGISTRY} \
     --registry-catalog ${REGISTRY} \
     --to-dir=${DIR} \
+
+  OPERATOR_VERSION=$(grep Version ./publish/mirror_log.txt | awk '{print $NF}' | tr -d ']')
 
   tar -czf "${operator}-${OPERATOR_VERSION}-mapping.tgz" ./publish
   rm -rf publish
